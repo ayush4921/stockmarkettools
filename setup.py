e83f95720ca2c94fc678d3445c0c17168f0edfa5
Ayush Garg <62711517+ayush4921@users.noreply.github.com>
2020-11-20 10:24:42 +0530

    Update

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ long_description = f.read()
 setup(
   name = 'stockmarkettools',         # How you named your package folder (MyLib)
   packages = ['stockmarkettools'],   # Chose the same as "name"
-  version = '0.2.4',      # Start with a small number and increase it with every change you make
+  version = '0.2.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'One stop solution for all your python stock market projects',   # Give a short description about your library
   author = 'Ayush Garg',                   # Type in your name
   author_email = 'themoviechannel77@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/ayush4921/stockmarkettools',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ayush4921/stockmarkettools/archive/0.2.4.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ayush4921/stockmarkettools/archive/0.2.5.tar.gz',    # I explain this later on
   keywords = ['Stocks','stock scraping','stock graphs','stock prediction'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
